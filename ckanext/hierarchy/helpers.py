9da87c0d0bdf73f3700566c6ef75e967e89c78c7
Lucia Espona <lucia.espona@wsl.ch>
2016-09-21 17:37:59 +0200

    fixed issue in the user dashboard by filtering the tree

--- ckanext/hierarchy/helpers.py
+++ ckanext/hierarchy/helpers.py
@@ -2,8 +2,39 @@ import ckan.plugins as p
 import ckan.model as model
 from ckan.common import request
 
-def group_tree(type_='organization'):
-    return p.toolkit.get_action('group_tree')({}, {'type': type_})
+def group_tree(organizations=[], type_='organization'):
+    full_tree_list = p.toolkit.get_action('group_tree')({}, {'type': type_})
+
+    if not organizations:
+        return full_tree_list
+    else:
+        filtered_tree_list = group_tree_filter(organizations, full_tree_list)
+        return filtered_tree_list
+
+
+def group_tree_filter(organizations, group_tree_list, highlight=False):
+    # this method leaves only the sections of the tree corresponding to the list
+    # since it was developed for the users, all children organizations from the 
+    # organizations in the list are included
+    def traverse_select_highlighted(group_tree, selection=[], highlight=False):
+        # add highlighted branches to the filtered tree
+        if group_tree['highlighted']:
+            # add to the selection and remove highlighting if necessary
+            if highlight:
+                selection += [group_tree]
+            else:
+                selection += group_tree_highlight([], [group_tree])
+        else:
+            # check if there is any highlighted child tree
+            for child in group_tree.get('children', []):
+                traverse_select_highlighted(child, selection)
+
+    filtered_tree=[]
+    # first highlights all the organizations from the list in the three
+    for group in group_tree_highlight(organizations, group_tree_list):
+        traverse_select_highlighted(group, filtered_tree, highlight)
+
+    return filtered_tree
 
 
 def group_tree_section(id_, type_='organization', include_parents=True, include_siblings=True):
@@ -31,6 +62,8 @@ def group_tree_highlight(organizations, group_tree_list):
     def traverse_highlight(group_tree, name_list):
         if group_tree.get('name', "") in name_list:
             group_tree['highlighted'] = True
+        else:
+            group_tree['highlighted'] = False
         for child in group_tree.get('children', []):
             traverse_highlight(child, name_list)
 
