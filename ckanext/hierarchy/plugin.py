05ea22b23858033417d647b571799ef3951d873a
Jari Voutilainen <jari.voutilainen@iki.fi>
2019-03-14 15:33:52 +0200

    Apply flake8

--- ckanext/hierarchy/plugin.py
+++ ckanext/hierarchy/plugin.py
@@ -2,9 +2,7 @@ import ckan.plugins as p
 from ckanext.hierarchy.logic import action
 from ckanext.hierarchy import helpers
 from ckan.lib.plugins import DefaultOrganizationForm
-from ckan.lib.plugins import DefaultGroupForm
-import ckan.logic.schema as s
-from ckan.common import c, request
+from ckan.common import c
 import logging
 import re
 
@@ -13,8 +11,8 @@ log = logging.getLogger(__name__)
 # This plugin is designed to work only these versions of CKAN
 p.toolkit.check_ckan_version(min_version='2.0')
 
-def custom_convert_from_extras(key, data, errors, context):
 
+def custom_convert_from_extras(key, data, errors, context):
     '''Converts values from extras, tailored for groups.'''
 
     # Set to empty string to remove Missing objects
@@ -24,18 +22,18 @@ def custom_convert_from_extras(key, data, errors, context):
     for data_key in data.keys():
         if (data_key[0] == 'extras'):
             data_value = data[data_key]
-            if( 'key' in data_value and data_value['key'] == key[-1]):
-               data[key] = data_value['value']
-               to_remove.append(data_key)
-               break
+            if ('key' in data_value and data_value['key'] == key[-1]):
+                data[key] = data_value['value']
+                to_remove.append(data_key)
+                break
     else:
         return
 
     for remove_key in to_remove:
         del data[remove_key]
 
-class HierarchyDisplay(p.SingletonPlugin):
 
+class HierarchyDisplay(p.SingletonPlugin):
     p.implements(p.IConfigurer, inherit=True)
     p.implements(p.IActions, inherit=True)
     p.implements(p.ITemplateHelpers, inherit=True)
@@ -67,7 +65,6 @@ class HierarchyDisplay(p.SingletonPlugin):
                 'is_include_children_selected': helpers.is_include_children_selected,
                 }
 
-
     # IPackageController
     # Modify the search query to include the datasets from
     # the children organizations in the result list
@@ -88,22 +85,21 @@ class HierarchyDisplay(p.SingletonPlugin):
         # fix the issues with multiple times repeated fields
         # remove the param from the fields
         new_fields = set()
-        for field,value in c.fields:
+        for field, value in c.fields:
             if (field != 'include_children'):
-                new_fields.add((field,value))
+                new_fields.add((field, value))
         c.fields = list(new_fields)
 
         # parse the query string to check if children are requested
         if query:
             base_query = []
             #  remove whitespaces between fields and values
-            query = re.sub(': +', ':',  query)
+            query = re.sub(': +', ':', query)
             for item in query.split(' '):
                 field = item.split(':')[0]
                 value = item.split(':')[-1]
-                # skip organization 
+                # skip organization
                 if (field == 'owner_org'):
-                    org_id = value
                     continue
                 # skip include children andset option value
                 if (field == 'include_children'):
@@ -113,26 +109,26 @@ class HierarchyDisplay(p.SingletonPlugin):
                 base_query += [item]
         if c.include_children_selected:
             # add all the children organizations in an 'or' join
-            children = _children_name_list(helpers.group_tree_section(c.group_dict.get('id'), include_parents=False, include_siblings=False).get('children',[]))
-            if(children):
+            children = _children_name_list(
+                helpers.group_tree_section(c.group_dict.get('id'), include_parents=False, include_siblings=False).get(
+                    'children', []))
+            if (children):
                 search_params['q'] = " ".join(base_query)
-                if (len(search_params['q'].strip())>0):
+                if (len(search_params['q'].strip()) > 0):
                     search_params['q'] += ' AND '
                 search_params['q'] += '(organization:%s' % c.group_dict.get('name')
                 for name in children:
                     if name:
-                        search_params['q'] += ' OR organization:%s' %  name
+                        search_params['q'] += ' OR organization:%s' % name
                 search_params['q'] += ")"
             # add it back to fields
-            c.fields += [('include_children','True')]
+            c.fields += [('include_children', 'True')]
 
         return search_params
 
 
 class HierarchyForm(p.SingletonPlugin, DefaultOrganizationForm):
-
     p.implements(p.IGroupForm, inherit=True)
-
 
     # IGroupForm
 
