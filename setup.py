8af2b4ba49cf50d9ac95d3883c679ab00cfac7bc
amercader <amercadero@gmail.com>
2022-08-18 10:57:28 +0200

    Set actual version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.1',
+    version='1.1.0',
 
     description='''CKAN Organization hierarchy - templates and configuration''',
     long_description=long_description,
