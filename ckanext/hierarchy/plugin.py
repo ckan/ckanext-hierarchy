66519fde013438c19998b24878fc6efd6abb7aac
alexmorev <aleksey.morev@linkdigital.com.au>
2022-10-26 00:32:12 +0300

    fix attrib error in test for CKAN v.2.7, 2.8

--- ckanext/hierarchy/plugin.py
+++ ckanext/hierarchy/plugin.py
@@ -140,8 +140,7 @@ class HierarchyDisplay(p.SingletonPlugin):
             # remove include_children from the filter-list - we have a checkbox
             g.fields_grouped.pop('include_children', None)
 
-        group_type = g.group_dict.get('type')
-        if group_type == 'group':
+        else:
             group_selected = model.Group.get(g.group_dict.get('id'))
             group_with_children = ' OR '.join(
                 '{}'.format(grp.name)
