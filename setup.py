c8df662d02ed02f8b7650bcd77e12d7ab2b3baa5
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2022-11-21 22:11:20 +0200

    Update version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.1.0',
+    version='1.2.0',
 
     description='''CKAN Organization hierarchy - templates and configuration''',
     long_description=long_description,
