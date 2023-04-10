1e6ef71348b8769900a8247ea10bddf7a205116d
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-04-10 20:26:56 +0300

    chore(package): bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.2.0',
+    version='1.2.1a0',
 
     description='''CKAN Organization hierarchy - templates and configuration''',
     long_description=long_description,
